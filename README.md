d53e9f560bfb9316ffe318dd95b184375cc74eea
coding_bot <68002888+DataSC3@users.noreply.github.com>
2023-04-19 06:51:55 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+# NOBLACK-MAIL v:1.0.5
+
+### **Новая версия:**
+    * Поиск данных по номеру телефона;
+    * Поиск MNP данных телефона;
+    * Поиск IP данных телефона;
+    * Получение открытых конф. данных;
+    * Получение рейтинга.
+    —— 2023
+
+### **Правооблодатель:**
+    Разработчик: FELIX4
+    Команда: noblack.command
+    Канал: noblack_channel
+    —— TELEGRAM
+
+### **Отказ от ответственности:**
+    Мы, команда разработчиков, хотим обратить ваше внимание на то, что использование данного приложения несет определенные риски и может привести к негативным последствиям.
+
+    Приложение noblack-mail предназначено для получении данных с открытых источников.
+
+    Мы не несем ответственности за использование данного приложения в незаконных целях или для нарушения прав и свобод других лиц.
+    —— LICENSE
+
+---
+## **Termux:**
+```Bash
+1. pkg update -y && upgrade -y
+2. pkg install git -y python -y
+3. pip install requests bs4 
+4. git clone https://github.com/DataSC3/noblack-mail
+5. cd noblack-mail
+6. python3 noblack-mail.py
+```
+
+##### ИЛИ МОЖЕТЕ СКОИРОВАТЬ ЭТО И ВСТАВИТЬ. 
+```Bash
+pkg update -y && upgrade -y && pkg install git -y python -y && pip install requests bs4 && git clone https://github.com/DataSC3/noblack-mail.git && cd noblack-mail && python3 noblack-mail.py
+```
+---
+## **Linux:**
+```Bash
+sudo apt-get install git 
+sudo apt-get install python3
+pip3 install requests bs4 
+git clone https://github.com/DataSC3/No-BlackM
+cd No-BlackM
+python3 No-BlackMail.py
+```
+
+##### ИЛИ МОЖЕТЕ СКОИРОВАТЬ ЭТО И ВСТАВИТЬ. 
+```Bash
+sudo apt-get install git && sudo apt-get install python3 &&pip3 install requests bs4 && git clone https://github.com/DataSC3/noblack-mail.git && cd noblack-mail && python3 noblack-mail.py
+```
+---
+## **Windows (cmd):**
+- Установите Python 3:\
+[Скачать Python можно тут.](https://www.python.org/downloads/)  ВАЖНО: поставьте галочку **"Add to PATH"** при установке, после установки, откройте консоль и введите:
+
+```Bash
+pip install requests bs4
+```
+---
+
+## **Запуск:**
+- Скачайте [репозиторий](https://github.com/DataSC3/noblack-mail/archive/master.zip) и распакуйте в удобное место.
+
+- Перейдите в папку со скриптом откройте в этой папке консоль `Shift + ПКМ`
+- Запустите скрипт командой `python noblack-mail.py`**
---
+++ noblack-mail.py
@@ -0,0 +1,80 @@
+# -*- coding UTF-8 -*-
+# КОММАНДА: noblack.command
+# НОВОСТИ: t.me/noblack_channel
+# АВТОР: FELIX4
+# DATE: Вторник, 18 Апреля 2023 г. 03:39:15 (+03)
+
+
+try:
+    # Проверка на наличие модулей
+    from src.config import COLOR_CODE, GLOBAL_SOFT_INFO, console_clear, print_banner, print_welcome_text
+    from src.httpweb_ip import HttpWebIp
+    from src.httpweb_mnp import HttpWebMnp
+    from src.httpweb_number import HttpWebNumber
+    from src.noblack_auto import print_noblack_auto_text
+    import requests
+    import bs4 
+    import os
+
+except ImportError:
+    # Совет по установке модулей и выход
+    print(f'\n{COLOR_CODE["RED"]}{COLOR_CODE["BOLD"]}[!] {COLOR_CODE["DARK"]}ВНИМАНИЕ У ВАС ПРОБЛЕМКА, НО МЫ ЕГО РЕШИМ!{COLOR_CODE["RESET"]}')
+
+    print(f'{COLOR_CODE["RED"]}[+] {COLOR_CODE["YELLOW"]}Оригинально программное обеспечение находиться на: '+
+         f'{COLOR_CODE["CYAN"]}{GLOBAL_SOFT_INFO["SOFT_ORIGINAL_LINK"]}{COLOR_CODE["RESET"]}\n'+
+         f'{COLOR_CODE["RED"]}[+] {COLOR_CODE["YELLOW"]}'+
+         f'Мы в телеграмме: {COLOR_CODE["CYAN"]}{GLOBAL_SOFT_INFO["SOFT_ORIGINAL_CHANNEL"]}{COLOR_CODE["RESET"]}')
+    
+    exit(f'\n{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}У вас отсутствует модули: '+
+         f'{COLOR_CODE["CYAN"]}requests{COLOR_CODE["RESET"]} и/или {COLOR_CODE["CYAN"]}'+
+         f'bs4{COLOR_CODE["RESET"]}. {COLOR_CODE["RED"]}\n[*] {COLOR_CODE["YELLOW"]}'+
+         f'Напишите в терминал/консоль: {COLOR_CODE["GREEN"]}pip3 install requests bs4{COLOR_CODE["RESET"]}')
+
+
+if __name__ == "__main__":
+    # Показ текст соглашении
+    print_welcome_text()
+
+    # Показ баннера
+    print_banner()
+
+    # Меню управления
+    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[1] {COLOR_CODE["LI_G"]}'
+        f'Проверить {COLOR_CODE["RED"]}номер{COLOR_CODE["LI_G"]} телефона {COLOR_CODE["RESET"]}\n'
+        
+        f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[2] {COLOR_CODE["LI_G"]}'
+        f'Проверить {COLOR_CODE["RED"]}MNP{COLOR_CODE["LI_G"]} телефона {COLOR_CODE["RESET"]}\n'
+        
+        f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[3] {COLOR_CODE["LI_G"]}'
+        f'Проверить {COLOR_CODE["RED"]}IP{COLOR_CODE["LI_G"]} телефона {COLOR_CODE["RESET"]}\n'
+
+        f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[4] {COLOR_CODE["LI_G"]}'
+        f'Проверить {COLOR_CODE["RED"]}Гос. номер{COLOR_CODE["LI_G"]} автомобиля{COLOR_CODE["RESET"]}\n')
+
+
+    try:
+        # Выбор варианта поиска
+        user_chooice: str = input(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[–] {COLOR_CODE["LI_G"]}'
+            f'Выберите вариант поиска: {COLOR_CODE["RESET"]}').strip()
+        
+        # Поиск по номеру телефона
+        if not user_chooice or user_chooice == "1":
+            httpweb_number = HttpWebNumber()
+            httpweb_number.print_number_results
+
+        # Поиск MNP по номеру телефона
+        if user_chooice == "2":
+            httpweb_number = HttpWebMnp()
+            httpweb_number.print_mnp_results
+
+        # Поиск по IP
+        if user_chooice == "3":
+            httpweb_number = HttpWebIp()
+            httpweb_number.print_ip_results
+
+        if user_chooice == "4":
+            print_noblack_auto_text()
+
+
+    except KeyboardInterrupt:
+        print(f'\n{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}Вынужденная остановка работы! {COLOR_CODE["RESET"]}\n')
