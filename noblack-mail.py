c5a14b26b7a4e9af24a3e9d744e8578e2426338b
coding_bot <68002888+DataSC3@users.noreply.github.com>
2023-08-08 03:33:21 +0400

    Update noblack-mail.py

--- noblack-mail.py
+++ noblack-mail.py
@@ -7,20 +7,22 @@
 
 try:
     # Проверка на наличие модулей
-    from src.config import COLOR_CODE, GLOBAL_SOFT_INFO, console_clear, print_banner, print_welcome_text
+    from src.config import COLOR_CODE, GLOBAL_SOFT_INFO, print_banner, print_welcome_text
     from src.httpweb_ip import HttpWebIp
     from src.httpweb_mnp import HttpWebMnp
     from src.httpweb_number import HttpWebNumber
+    from src.blocked_countries import BlockedCountries
+    from src.update.update import Update
     from src.noblack_auto import print_noblack_auto_text
-    import requests
-    import bs4 
-    import os
+    from time import sleep
+    import requests, bs4
+
 
 except ImportError:
     # Совет по установке модулей и выход
     print(f'\n{COLOR_CODE["RED"]}{COLOR_CODE["BOLD"]}[!] {COLOR_CODE["DARK"]}ВНИМАНИЕ У ВАС ПРОБЛЕМКА, НО МЫ ЕГО РЕШИМ!{COLOR_CODE["RESET"]}')
 
-    print(f'{COLOR_CODE["RED"]}[+] {COLOR_CODE["YELLOW"]}Оригинальное программное обеспечение находиться на: '+
+    print(f'{COLOR_CODE["RED"]}[+] {COLOR_CODE["YELLOW"]}Оригинально программное обеспечение находиться на: '+
          f'{COLOR_CODE["CYAN"]}{GLOBAL_SOFT_INFO["SOFT_ORIGINAL_LINK"]}{COLOR_CODE["RESET"]}\n'+
          f'{COLOR_CODE["RED"]}[+] {COLOR_CODE["YELLOW"]}'+
          f'Мы в телеграмме: {COLOR_CODE["CYAN"]}{GLOBAL_SOFT_INFO["SOFT_ORIGINAL_CHANNEL"]}{COLOR_CODE["RESET"]}')
@@ -35,46 +37,68 @@ if __name__ == "__main__":
     # Показ текст соглашении
     print_welcome_text()
 
-    # Показ баннера
-    print_banner()
+    while True:
+        # Показ баннера
+        print_banner()
 
-    # Меню управления
-    print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[1] {COLOR_CODE["LI_G"]}'
-        f'Проверить {COLOR_CODE["RED"]}номер{COLOR_CODE["LI_G"]} телефона {COLOR_CODE["RESET"]}\n'
+        # Меню управления
+        print(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[1] {COLOR_CODE["LI_G"]}'
+            f'Проверить {COLOR_CODE["RED"]}номер{COLOR_CODE["LI_G"]} телефона.{COLOR_CODE["RESET"]}\n'
+            
+            f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[2] {COLOR_CODE["LI_G"]}'
+            f'Проверить {COLOR_CODE["RED"]}MNP{COLOR_CODE["LI_G"]} телефона.{COLOR_CODE["RESET"]}\n'
+            
+            f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[3] {COLOR_CODE["LI_G"]}'
+            f'Проверить {COLOR_CODE["RED"]}IP{COLOR_CODE["LI_G"]} телефона.{COLOR_CODE["RESET"]}\n'
+
+            f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[4] {COLOR_CODE["LI_G"]}'
+            f'Проверить {COLOR_CODE["RED"]}Гос. номер{COLOR_CODE["LI_G"]} автомобиля.{COLOR_CODE["RESET"]}\n'
+            
+            f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[5] {COLOR_CODE["LI_G"]}'
+            f'Провести техническую проверку {COLOR_CODE["RED"]}на работоспосбность сервиса.{COLOR_CODE["LI_G"]}{COLOR_CODE["RESET"]}\n')
+
+        try:
         
-        f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[2] {COLOR_CODE["LI_G"]}'
-        f'Проверить {COLOR_CODE["RED"]}MNP{COLOR_CODE["LI_G"]} телефона {COLOR_CODE["RESET"]}\n'
-        
-        f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[3] {COLOR_CODE["LI_G"]}'
-        f'Проверить {COLOR_CODE["RED"]}IP{COLOR_CODE["LI_G"]} телефона {COLOR_CODE["RESET"]}\n'
+            # Выбор варианта поиска
+            user_chooice: str = input(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[–] {COLOR_CODE["LI_G"]}'
+                f'Выберите вариант поиска: {COLOR_CODE["RESET"]}').strip()
+            
+            # Поиск по номеру телефона
+            if not user_chooice or user_chooice == "1":
+                httpweb_number = HttpWebNumber()
+                httpweb_number.print_number_results
+                sleep(3)
 
-        f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[4] {COLOR_CODE["LI_G"]}'
-        f'Проверить {COLOR_CODE["RED"]}Гос. номер{COLOR_CODE["LI_G"]} автомобиля{COLOR_CODE["RESET"]}\n')
+            # Поиск MNP по номеру телефона
+            elif user_chooice == "2":
+                httpweb_number = HttpWebMnp()
+                httpweb_number.print_mnp_results
+                sleep(3)
 
+            # Поиск по IP
+            elif user_chooice == "3":
+                httpweb_number = HttpWebIp()
+                httpweb_number.print_ip_results
+                sleep(3)
 
-    try:
-        # Выбор варианта поиска
-        user_chooice: str = input(f'{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[–] {COLOR_CODE["LI_G"]}'
-            f'Выберите вариант поиска: {COLOR_CODE["RESET"]}').strip()
-        
-        # Поиск по номеру телефона
-        if not user_chooice or user_chooice == "1":
-            httpweb_number = HttpWebNumber()
-            httpweb_number.print_number_results
+            # Поиск по Гос. номеру
+            elif user_chooice == "4":
+                print_noblack_auto_text()
 
-        # Поиск MNP по номеру телефона
-        if user_chooice == "2":
-            httpweb_number = HttpWebMnp()
-            httpweb_number.print_mnp_results
+            # Проверка тех. части
+            elif user_chooice == "5":
+                # Проверка на блокировку
+                BlockedCountries().print_ip_result()
+                
+                # Проверка обновлении
+                Update().get()
+                
+                input(f'\n{COLOR_CODE["CYAN"]}{COLOR_CODE["BOLD"]}[{COLOR_CODE["RED"]}!{COLOR_CODE["CYAN"]}] {COLOR_CODE["LI_G"]}' + 
+                  f'Чтобы вернуться назад, нажмите{COLOR_CODE["DARK"]} {COLOR_CODE["RESET"]}ENTER ')
 
-        # Поиск по IP
-        if user_chooice == "3":
-            httpweb_number = HttpWebIp()
-            httpweb_number.print_ip_results
+            # Повторный опрос
+            else: continue
 
-        if user_chooice == "4":
-            print_noblack_auto_text()
-
-
-    except KeyboardInterrupt:
-        print(f'\n{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}Вынужденная остановка работы! {COLOR_CODE["RESET"]}\n')
+        except KeyboardInterrupt:
+            print(f'\n{COLOR_CODE["RED"]}[!] {COLOR_CODE["YELLOW"]}Вынужденная остановка работы! {COLOR_CODE["RESET"]}\n')
+            break
